3fa214624841524a622a6c74a2ac2c0d60a0d21f
Gedson <58708778+GEDSONN@users.noreply.github.com>
2022-12-20 17:08:24 +0000

    Initial commit

---
+++ assets/js/checks.js
@@ -0,0 +1,6 @@
+if (navigator.userAgent !== "BDP (http://example.com), v0.0.1") {
+  warnNotification("Please set User Agent to 'BDP (http://example.com), v0.0.1'. Visit <a href='https://github.com/SanjaySunil/BetterDiscordPanel/blob/dev/docs/prerequisites/prerequisites.md'>here</a> for more information.")
+} else {
+  successNotification("All checks passed successfully.")
+}
+
